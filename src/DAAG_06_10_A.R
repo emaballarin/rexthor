70cfa7cdcdb7309e1b4e341a1dacfe2065e71bae
wilsongefferson <pierpa.m.96live.it>
2020-05-25 16:13:42 +0200

    ex 8.2 added, Rmd modified (ex 6.10 and 8.2)

---
+++ src/DAAG_06_10_A.R
@@ -0,0 +1,14 @@
+library(MPV)
+library(lattice)
+
+dataset <- table.b3
+
+plot_fit <- function(fitted_model, x, y, xlab, subtitle){
+  plot(x, y, main = "fitted model", xlab = xlab, sub = subtitle)
+  lines(x, as.vector(fitted_model$fitted.values), col ="red")
+  text(13,3, "y = b0+b1*x", col="red")
+}
+
+# scatter plot
+plot(dataset$x1, dataset$y, main="Scatterplot x1 versus y",
+     xlab="x_1 (displacement)", ylab="y (mpg)", pch=19)
--- src/DAAG_08_02.R
+++ src/DAAG_08_02.R
@@ -5,6 +5,10 @@ library(DAAG)
 # load dataset
 data <- head.injury
 
+# exploiting data
+str(data)
+summary(data)
+
 # perform glm
 model_glm <- glm(clinically.important.brain.injury ~ ., 
                  family = binomial(link = "logit"),
@@ -12,3 +16,22 @@ model_glm <- glm(clinically.important.brain.injury ~ .,
 
 #summary
 summary(model_glm)
+
+# turn the result into a decision rule for use of CT
+judge_function <- function(patient, threshold){
+  n_coeff <- length(model_glm$coefficients) # number of estimated coefficients
+  
+  p <- model_glm$coefficients[1] + patient %*% model_glm$coefficients[2:n_coeff]
+  logistic_prob <- exp(p) / (1 + exp(p))
+  
+  return(logistic_prob < threshold) # return the if condition on the threshold
+}
+
+# check correctness of the judge function
+x <-  sapply(data,'[[', 1)[-11] # a possible patient
+
+if(judge_function(patient = x, threshold = 0.025)){
+  print("Patient should not being sent to CT")
+}else{
+  print("Patient should being sent to CT")
+}
